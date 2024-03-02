708b8c6a0cbb2ceef5f890747700d32a5cb2ab22
Daniel Suárez Tibaquira <dantibaquira@gmail.com>
2024-03-02 07:44:37 -0500

    Inicio

---
+++ main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <div>
+            <h1>Main Component</h1>
+            <p>This is the main component of your React application.</p>
+        </div>
+    );
+}
+
+export default Main;
